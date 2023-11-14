da1c075f8906799dfba06faf6ef2aee32a2f36da
xarix9 <ariesse63@gmail.com>
2023-11-14 16:01:15 +0100

    first commit

---
+++ Bookapp/Bookapp/BookappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookappApp.swift
+//  Bookapp
+//
+//  Created by Arianna Spirito on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BookappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
